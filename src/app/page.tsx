56328744889a92b4e8d8f76c5a1c204bcc5c5de8
Peggy Lu <lupeggy0608@gmail.com>
2025-06-24 23:05:59 +1000

    sign in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
-import Image from "next/image";
+"use client"
+
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Hello World!
+    <div className="p-4 flex flex-col gap-4">
+      <Input 
+        placeholder="name" 
+        value={name} 
+        onChange={(e) => setName(e.target.value)} />
+      <Input 
+        placeholder="email" 
+        value={email} 
+        onChange={(e) => setEmail(e.target.value)} />
+      <Input 
+        placeholder="password" 
+        value={password} 
+        onChange={(e) => setPassword(e.target.value)} />
+
+      <Button onClick={() => {}}>Create User</Button>
     </div>
   );
 }
