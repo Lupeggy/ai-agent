f6059600ac1c2f0e99a7ea20763b34d2626ba1e5
Peggy Lu <lupeggy0608@gmail.com>
2025-06-26 21:30:47 +1000

    agent back to sign in when unauth

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -2,9 +2,9 @@ import { Suspense } from "react";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { ErrorBoundary } from "react-error-boundary";
-// import { redirect } from "next/navigation";
-// import { headers } from "next/headers";
-// import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
 
 import { AgentListHeader } from "@/modules/agents/ui/components/agent-list-header";
 
@@ -15,9 +15,15 @@ import {
 } from "@/modules/agents/ui/views/agents-view";
 
 
-
-
 const Page = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/sign-in");
+  }
+
   // Check if user is authenticated
   const queryClient = getQueryClient();
   void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
